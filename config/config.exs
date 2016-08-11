ef020ac43c80e4541bb54a39cc851db91a493b9a
Adrian Dunston <adrian@spreedly.com>
2016-08-10 21:23:38 -0400

    Bump version number

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 
 # Sample configuration:
-# config :mix_test_notify, :sound, true
+config :mix_test_notify, :sound, true
 # config :mix_test_notify, :win_sound, "Blow"
 # config :mix_test_notify, :fail_sound, "Basso"
 # config :mix_test_notify, :win_title, "Win"
