515b99a1283289c926ee3ba54e0832781d11e180
Adrian Dunston <adrian@spreedly.com>
2016-09-06 13:47:26 -0400

    Adding docs package.

--- mix.exs
+++ mix.exs
@@ -30,7 +30,8 @@ defmodule MixTestNotify.Mixfile do
     [
       {:mix_test_watch, "~> 0.2", only: :dev},
       {:dogma, "~> 0.1", only: :dev},
-      {:notifier, "~> 0.0.1"}
+      {:notifier, "~> 0.0.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 
